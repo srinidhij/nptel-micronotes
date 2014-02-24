2a621c5830d4902d275c92b1e600fe116e5cb670
Srinidhi J <srinidhij.21@gmail.com>
2014-02-24 04:36:23 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,50 +1,128 @@
+'use strict';
 
-/**
- * Module dependencies.
- */
+//dependencies
+var config = require('./config'),
+    express = require('express'),
+    mongoStore = require('connect-mongo')(express),
+    http = require('http'),
+    path = require('path'),
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    helmet = require('helmet');
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
+//create express app
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.cookieParser('wow, such secret'));
-app.use(express.session());
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//keep reference to config
+app.config = config;
 
-// development only
-if ('development' == app.get('env')) {
+//setup the web server
+app.server = http.createServer(app);
+
+//setup mongoose
+app.db = mongoose.createConnection(config.mongodb.uri);
+app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
+app.db.once('open', function () {
+  //and... we have a data store
+});
+
+//config data models
+require('./models')(app, mongoose);
+
+//setup the session store
+app.sessionStore = new mongoStore({ url: config.mongodb.uri });
+
+//config express in all environments
+app.configure(function(){
+  //settings
+  app.disable('x-powered-by');
+  app.set('port', config.port);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('strict routing', true);
+  app.set('project-name', config.projectName);
+  app.set('company-name', config.companyName);
+  app.set('system-email', config.systemEmail);
+  app.set('crypto-key', config.cryptoKey);
+  app.set('require-account-verification', config.requireAccountVerification);
+
+  //smtp settings
+  app.set('smtp-from-name', config.smtp.from.name);
+  app.set('smtp-from-address', config.smtp.from.address);
+  app.set('smtp-credentials', config.smtp.credentials);
+
+  //twitter settings
+  app.set('twitter-oauth-key', config.oauth.twitter.key);
+  app.set('twitter-oauth-secret', config.oauth.twitter.secret);
+
+  //github settings
+  app.set('github-oauth-key', config.oauth.github.key);
+  app.set('github-oauth-secret', config.oauth.github.secret);
+
+  //facebook settings
+  app.set('facebook-oauth-key', config.oauth.facebook.key);
+  app.set('facebook-oauth-secret', config.oauth.facebook.secret);
+
+  //google settings
+  app.set('google-oauth-key', config.oauth.google.key);
+  app.set('google-oauth-secret', config.oauth.google.secret);
+
+  //middleware
+  app.use(express.logger('dev'));
+  app.use(express.compress());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.urlencoded());
+  app.use(express.json());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: config.cryptoKey,
+    store: app.sessionStore
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  helmet.defaults(app);
+
+  //response locals
+  app.use(function(req, res, next) {
+    res.locals.user = {};
+    res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
+    res.locals.user.username = req.user && req.user.username;
+    next();
+  });
+
+  //mount the routes
+  app.use(app.router);
+
+  //error handler
+  app.use(require('./views/http/index').http500);
+
+  //global locals
+  app.locals.projectName = app.get('project-name');
+  app.locals.copyrightYear = new Date().getFullYear();
+  app.locals.copyrightName = app.get('company-name');
+  app.locals.cacheBreaker = 'br34k-01';
+});
+
+//config express in dev environment
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
+});
 
-app.get('/', routes.index);
-app.get('/create_account', routes.create_account);
-app.post('/send_login', routes.send_login);
-app.post('/login', routes.login);
-app.post('/submit_note', routes.note_submit);
-app.get('/home', routes.home);
-app.get('/download', routes.download_notes);
-app.get('/addnote', routes.addnote);
-app.get('/query', routes.query);
-app.get('/search', routes.search);
-app.get('/view_note',routes.view_note);
-app.post('/add_comment',routes.add_comment);
-app.post('/rate_note', routes.rate_note);
+//setup passport
+require('./passport')(app, passport);
 
+//route requests
+require('./routes')(app, passport);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+//setup utilities
+app.utility = {};
+app.utility.sendmail = require('drywall-sendmail');
+app.utility.slugify = require('drywall-slugify');
+app.utility.workflow = require('drywall-workflow');
+
+//listen up
+app.server.listen(app.get('port'), function(){
+  //and... we're live
 });
--- subjects.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-	{
-		name : "Theory of Computation",
-		course_id : 106104028,
-		no_of_lectures : 33
-	}
-]
-
